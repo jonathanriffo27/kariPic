2ff89afaff3996e6c63b3849634aeb87597ebe09
Jonathan <jonathan.riffo7@gmail.com>
2023-08-24 18:32:50 -0300

    Generacion modelo Post Comment

---
+++ db/migrate/20230824211006_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.string :image
+      t.string :context
+
+      t.timestamps
+    end
+  end
+end
